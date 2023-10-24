5dc738e2c25d3c505f6176fdebb0eff4c67e989b
jason-sparks <hijasonsparks@gmail.com>
2023-10-24 10:38:48 -0700

    Initial commit - module 2 of pluralsight course, Setting up a Java 17 Application, completed.

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,26 @@
+package com.pluralsight.courseinfo.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String... args) {
+        LOG.info("CourseRetriever starting...");
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as first argument.");
+            return;
+        }
+
+        try {
+            retrieveCourse(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected error", e);
+        }
+    }
+
+    private static void retrieveCourse(String authorId) {
+        LOG.info("Retrieving courses for author '{}'", authorId);
+    }
+}
